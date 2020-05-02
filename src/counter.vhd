0ba83a8cdd3dcf98983500a9417f330494ee2523
Joshua Bassett <hello@joshbassett.info>
2020-05-02 10:23:29 +1000

    Add clock divider

--- src/counter.vhd
+++ src/counter.vhd
@@ -11,12 +11,19 @@ end counter;
 
 architecture arch of counter is
   signal n : natural range 0 to 255;
+  signal cen : std_logic;
 begin
+  clock_divider : entity work.clock_divider
+  generic map (DIVISOR => 5000000)
+  port map (clk => clk, cen => cen);
+
   counter : process(clk)
   begin
     if rising_edge(clk) then
-      n <= n + 1;
-      led <= std_logic_vector(to_unsigned(n, led'length));
+      if cen = '1' then
+        n <= n + 1;
+        led <= std_logic_vector(to_unsigned(n, led'length));
+      end if;
     end if;
   end process;
 end arch;
